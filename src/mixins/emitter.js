e0c581b65382039e5f5b42fbdac2ab4e3da6d8b2
XShinei <497449921@qq.com>
2018-11-29 22:01:27 +0800

    Select component watch visible

--- src/mixins/emitter.js
+++ src/mixins/emitter.js
@@ -1,3 +1,15 @@
+function broadcast(componentName, eventName, params) {
+    this.$children.forEach(child => {
+        var name = child.$options.componentName;
+
+        if (name === componentName) {
+            child.$emit.apply(child, [eventName].concat(params))
+        }
+        else {
+            broadcast.apply(child, [componentName, eventName].concat([params]));
+        }
+    });
+}
 
 export default {
     methods: {
@@ -16,6 +28,9 @@ export default {
             if (parent) {
                 parent.$emit.apply(parent, [eventName].concat(params));
             }
+        },
+        broadcast(componentName, eventName, params) {
+            broadcast.call(this, componentName, eventName, params);
         }
     }
 };
--- src/packages/select/src/select.vue
+++ src/packages/select/src/select.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="el-select" 
         :class="[selectSize ? 'el-select--' + selectSize : '']">
+        <div >
+
+        </div>
         <el-input type="text" 
-                    v-model="selectedLabel" 
+                    v-model="selectedLabel"
+                    :placeholder="currentPlaceholder"
+                    :name="name"
+                    :id="id"
+                    :autocomplete="autocomplete"
+                    :size="selectSize"
+                    :disabled="selectDisabled"
+                    :readonly="readonly"
+                    :validate-event="false"
+                    :class="{'is-icon': visible}" 
                     ref="reference">
 
         </el-input>
@@ -11,11 +23,14 @@
 
 <script>
     import ElInput from '../../../packages/input/index';
+    import Emitter from '../../../mixins/emitter';
 
     export default {
         name: 'ElSelect',
 
         componentName: 'ElSelect',
+
+        mixins: [Emitter],
 
         props: {
             name: String,       // select input 的 name 属性
@@ -90,13 +105,77 @@
 
         data() {
             return {
-                selectedLabel: ''
+                selectedLabel: '',
+                visible: false,         // 下拉框出现/隐藏
+                hoverIndex: -1,         // 选中 的 下拉选项 的 索引
+                selected: this.multiple ? [] : {},      // 已选中的拉下选项
+                currentPlaceholder: '',
+                cachedPlaceholder: ''
             };
         },
 
         computed: {
             selectSize() {
                 return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
+            },
+
+            selectDisabled() {
+                return this.disabled || (this.elForm || {}).disabled;
+            }
+        },
+
+        methods: {
+            resetHoverIndex() {
+                setTimeout(() => {
+                    // 单选
+                    if (!this.multiple) {
+                        this.hoverIndex = this.options.indexOf(this.selected);
+                    }
+                    else {
+                        // 多选
+
+                        // 选中的数目大于0
+                        if (this.selected.length > 0) {
+                            // 取选中的选项 中 最小的索引
+                            this.hoverIndex = Math.min.apply(null, this.selected.map(item => this.options.indexOf(item)));
+                        }
+                        else {
+                            this.hoverIndex = -1;
+                        }
+                    }
+                });
+            }
+        },
+
+        watch: {
+            visible(val) {
+                if (!val) {
+                    // 触发该组件下 所有后代组件名为 ElSelectDropdown 的组件 的  destoryPopper 事件
+                    this.broadcast('ElSelectDropdown', 'destoryPopper');
+
+                    // 输入框失去焦点
+                    if (this.$refs.input) {
+                        this.$refs.input.blur();
+                    }
+
+                    this.query = '';
+                    this.previousQuery = null;
+                    this.selectedLabel = '';
+                    this.inputLength = 20;
+
+                    // 重置 选中的下拉选项索引
+                    this.resetHoverIndex();
+
+                    this.$nextTick(() => {
+                        if (this.$refs.input && this.$refs.input.value === '' && this.selected.length === 0) {
+                            this.currentPlaceholder = this.cachedPlaceholder;
+                        }
+                    });
+                }
+            },
+
+            placeholder(val) {
+                this.cachedPlaceholder = this.currentPlaceholder = val;
             }
         }
     }
